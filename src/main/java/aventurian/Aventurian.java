1a461e06afdbdfaf562001176820d84701ac53b4
JMoersch <JMoersch@wegwerfemail.de>
2017-11-07 16:58:20 +0100

    more streams :)

--- src/main/java/aventurian/Aventurian.java
+++ src/main/java/aventurian/Aventurian.java
@@ -3,121 +3,116 @@ package aventurian;
 import skills.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Aventurian {
 
-    private String name;
-    private PrimaryAttributes primaryAttributes;
-    private SecondaryAttributes secondaryAttributes;
-    private int adventurePoints;
+	private String name;
+	private PrimaryAttributes primaryAttributes;
+	private SecondaryAttributes secondaryAttributes;
+	private int adventurePoints;
+
+	private final List<Property> properties;
+	private final List<BadProperty> badProperties;
+	private final List<Language> languages;
+
+	Aventurian(String name, int ap) {
+		this.name = name;
+		this.primaryAttributes = new PrimaryAttributes();
+		this.secondaryAttributes = new SecondaryAttributes();
+		this.adventurePoints = ap;
+		this.properties = new ArrayList<>();
+		this.badProperties = new ArrayList<>();
+		this.languages = new ArrayList<>();
+	}
+
+	Aventurian(int ap) {
+		this("", ap);
+	}
+
+	public int getAdventurePoints() {
+		return adventurePoints;
+	}
+
+	void pay(int cost) {
+		if (canPay(cost) && cost >= 0)
+			adventurePoints -= cost;
+		else
+			throw new IllegalArgumentException("Cannot pay: " + cost);
+	}
+
+	void refund(int refund) {
+		if (refund >= 0)
+			adventurePoints += refund;
+		else
+			throw new IllegalArgumentException(
+					"Cannot refund negative amound: " + refund);
+	}
+
+	boolean canPay(int cost) {
+		return cost <= adventurePoints;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	void add(Property p) {
+		properties.add(p);
+	}
+
+	void remove(Property p) {
+		properties.remove(p);
+	}
+
+	void add(BadProperty p) {
+		badProperties.add(p);
+	}
+
+	void remove(BadProperty p) {
+		badProperties.remove(p);
+	}
+
+	int getBadPropertySum() {
+		return badProperties.stream().mapToInt(BadProperty::getLevel).sum();
+	}
+
+	void add(Language l) {
+		languages.add(l);
+	}
+
+	void remove(Language l) {
+		languages.remove(l);
+	}
+
+	boolean hasSkill(Skill skill) {
+		return Stream.of(badProperties, properties, languages)
+				.flatMap(Collection::stream).anyMatch((s) -> s.equals(skill));
+	}
 
 
-    private final List<Property> properties;
-    private final List<BadProperty> badProperties;
-    private final List<Language> languages;
+	int getSumOfPrimaryAttributes() {
+		return primaryAttributes.getSum();
+	}
 
-    Aventurian(String name, int ap) {
-        this.name = name;
-        this.primaryAttributes = new PrimaryAttributes();
-        this.secondaryAttributes = new SecondaryAttributes();
-        this.adventurePoints = ap;
-        this.properties = new ArrayList<>();
-        this.badProperties = new ArrayList<>();
-        this.languages = new ArrayList<>();
-    }
+	int getPrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE a) {
+		return primaryAttributes.getPrimaryAttribute(a);
+	}
 
-    Aventurian(int ap) {
-        this("", ap);
-    }
+	int getMaxOfPrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE a) {
+		return primaryAttributes.getMaximumOfPrimaryAttribute(a);
+	}
 
-    public int getAdventurePoints() {
-        return adventurePoints;
-    }
+	void increasePrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE attribute) {
+		primaryAttributes.increase(attribute);
+		secondaryAttributes.updateValues(primaryAttributes);
+	}
 
-    void pay(int cost) {
-        if (canPay(cost) && cost >= 0) adventurePoints -= cost;
-        else throw new IllegalArgumentException("Cannot pay: " + cost);
-    }
-
-    void refund(int refund) {
-        if (refund >= 0) adventurePoints += refund;
-        else throw new IllegalArgumentException("Cannot refund negative amound: " + refund);
-    }
-
-    boolean canPay(int cost) {
-        return cost <= adventurePoints;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    void addProperty(Property p) {
-        properties.add(p);
-    }
-
-    void removeProperty(Property p) {
-        properties.remove(p);
-    }
-
-    void addBadProperty(BadProperty p) {
-        badProperties.add(p);
-    }
-
-    void removeBadProperty(BadProperty p) {
-        badProperties.remove(p);
-    }
-
-    int getBadPropertySum() {
-        return badProperties.stream().mapToInt(BadProperty::getLevel).sum();
-    }
-
-    void addLanguage(Language l) {
-        languages.add(l);
-    }
-
-    void removeLanguage(Language l) {
-        languages.remove(l);
-    }
-
-    boolean hasSkill(Skill skill) {
-        for (Property p : properties) {
-            if (p.equals(skill)) return true;
-        }
-        for (Language l : languages) {
-            if (l.equals(skill)) return true;
-        }
-        return false;
-    }
-
-    boolean hasProperty(Property p) {
-        return properties.contains(p);
-    }
-
-    boolean hasLanguage(Language l) {
-        return languages.contains(l);
-    }
-
-    int getSumOfPrimaryAttributes() {
-        return primaryAttributes.getSum();
-    }
-
-    int getPrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE a) {
-        return primaryAttributes.getPrimaryAttribute(a);
-    }
-
-    int getMaxOfPrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE a) {
-        return primaryAttributes.getMaximumOfPrimaryAttribute(a);
-    }
-
-    void increasePrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE attribute) {
-        primaryAttributes.increase(attribute);
-        secondaryAttributes.updateValues(primaryAttributes);
-    }
-
-    void decrasePrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE attribute) {
-        primaryAttributes.decrease(attribute);
-        secondaryAttributes.updateValues(primaryAttributes);
-    }
+	void decrasePrimaryAttribute(PrimaryAttributes.PRIMARY_ATTRIBUTE attribute) {
+		primaryAttributes.decrease(attribute);
+		secondaryAttributes.updateValues(primaryAttributes);
+	}
 }
--- src/test/java/aventurian/AventurianTest.java
+++ src/test/java/aventurian/AventurianTest.java
@@ -3,136 +3,135 @@ package aventurian;
 import org.junit.Before;
 import org.junit.Test;
 
+import aventurian.PrimaryAttributes.PRIMARY_ATTRIBUTE;
 import static org.mockito.Mockito.*;
-
 import skills.Property;
-
 
 import static org.junit.Assert.*;
 
 public class AventurianTest {
 
-    public static final int AP = 16500;
-    private Aventurian toTest;
+	public static final int AP = 16500;
+	private Aventurian toTest;
 
+	@Before
+	public void setUp() throws Exception {
+		toTest = new Aventurian("test", AP);
+	}
 
-    @Before
-    public void setUp() throws Exception {
-        toTest = new Aventurian("test", AP);
-    }
+	@Test
+	public void payValid() throws Exception {
+		toTest.pay(1000);
+		int expected = AP - 1000;
+		int actual = toTest.getAdventurePoints();
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void payValid() throws Exception {
-        toTest.pay(1000);
-        int expected = AP - 1000;
-        int actual = toTest.getAdventurePoints();
-        assertEquals(expected, actual);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void payToMuch() throws Exception {
+		toTest.pay(20000);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void payToMuch() throws Exception {
-        toTest.pay(20000);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void payNegative() throws Exception {
+		toTest.pay(-20);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void payNegative() throws Exception {
-        toTest.pay(-20);
-    }
+	@Test
+	public void refundValid() throws Exception {
+		toTest.refund(500);
+		int expected = AP + 500;
+		int actual = toTest.getAdventurePoints();
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void refundValid() throws Exception {
-        toTest.refund(500);
-        int expected = AP + 500;
-        int actual = toTest.getAdventurePoints();
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void refundMuch() throws Exception {
+		toTest.refund(20000);
+		int expected = AP + 20000;
+		int actual = toTest.getAdventurePoints();
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void refundMuch() throws Exception {
-        toTest.refund(20000);
-        int expected = AP + 20000;
-        int actual = toTest.getAdventurePoints();
-        assertEquals(expected, actual);
-    }
+	@Test(expected = IllegalArgumentException.class)
+	public void refundNegative() throws Exception {
+		toTest.refund(-20);
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void refundNegative() throws Exception {
-        toTest.refund(-20);
-    }
+	@Test
+	public void canPay() throws Exception {
+		assertFalse(toTest.canPay(20000));
+	}
 
-    @Test
-    public void canPay() throws Exception {
-        assertFalse(toTest.canPay(20000));
-    }
+	@Test
+	public void addProperty() throws Exception {
+		Property testProp = mock(Property.class);
+		when(testProp.getName()).thenReturn("test");
+		toTest.add(testProp);
+		assertTrue(toTest.hasSkill(testProp));
+	}
 
-    @Test
-    public void addProperty() throws Exception {
-        Property testProp = mock(Property.class);
-        when(testProp.getName()).thenReturn("test");
-        toTest.addProperty(testProp);
-        assertTrue(toTest.hasSkill(testProp));
-    }
+	@Test
+	public void removeProperty() throws Exception {
+		Property testProp = mock(Property.class);
+		when(testProp.getName()).thenReturn("test");
+		toTest.add(testProp);
+		assertTrue(toTest.hasSkill(testProp));
+		toTest.remove(testProp);
+		assertFalse(toTest.hasSkill(testProp));
+	}
 
-    @Test
-    public void removeProperty() throws Exception {
-        Property testProp = mock(Property.class);
-        when(testProp.getName()).thenReturn("test");
-        toTest.addProperty(testProp);
-        assertTrue(toTest.hasSkill(testProp));
-        toTest.removeProperty(testProp);
-        assertFalse(toTest.hasSkill(testProp));
-    }
+	@Test
+	public void addBadProperty() throws Exception {
+	}
 
-    @Test
-    public void addBadProperty() throws Exception {
-    }
+	@Test
+	public void removeBadProperty() throws Exception {
+	}
 
-    @Test
-    public void removeBadProperty() throws Exception {
-    }
+	@Test
+	public void getBadPropertySum() throws Exception {
+	}
 
-    @Test
-    public void getBadPropertySum() throws Exception {
-    }
+	@Test
+	public void addLanguage() throws Exception {
+	}
 
-    @Test
-    public void addLanguage() throws Exception {
-    }
+	@Test
+	public void removeLanguage() throws Exception {
+	}
 
-    @Test
-    public void removeLanguage() throws Exception {
-    }
+	@Test
+	public void hasSkill() throws Exception {
+	}
 
-    @Test
-    public void hasSkill() throws Exception {
-    }
+	@Test
+	public void hasProperty() throws Exception {
+	}
 
-    @Test
-    public void hasProperty() throws Exception {
-    }
+	@Test
+	public void hasLanguage() throws Exception {
+	}
 
-    @Test
-    public void hasLanguage() throws Exception {
-    }
+	@Test
+	public void getSumOfPrimaryAttributes() throws Exception {
+	}
 
-    @Test
-    public void getSumOfPrimaryAttributes() throws Exception {
-    }
+	@Test
+	public void getPrimaryAttribute() throws Exception {
+		assertEquals(8, toTest.getPrimaryAttribute(PRIMARY_ATTRIBUTE.COURAGE));
+	}
 
-    @Test
-    public void getPrimaryAttribute() throws Exception {
-    }
+	@Test
+	public void getMaxOfPrimaryAttribute() throws Exception {
+	}
 
-    @Test
-    public void getMaxOfPrimaryAttribute() throws Exception {
-    }
+	@Test
+	public void increasePrimaryAttribute() throws Exception {
+	}
 
-    @Test
-    public void increasePrimaryAttribute() throws Exception {
-    }
-
-    @Test
-    public void decrasePrimaryAttribute() throws Exception {
-    }
+	@Test
+	public void decrasePrimaryAttribute() throws Exception {
+	}
 
 }
